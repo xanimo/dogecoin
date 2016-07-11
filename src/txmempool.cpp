99c1feff90c3de27bae24c54fee683aa4d8eef6b
Jorge Timón <jtimon@jtimon.cc>
2016-07-11 16:34:21 +0200

    MOVEONLY: tx functions to consensus/tx_verify.o
    Functions related to transaction verification.
    
    Cherry-picked from: 618d07faa2cc2f061a2e8035c3edbffc192480d7

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -9,6 +9,7 @@
 #include "chainparams.h"
 #include "clientversion.h"
 #include "consensus/consensus.h"
+#include "consensus/tx_verify.h"
 #include "consensus/validation.h"
 #include "validation.h"
 #include "policy/policy.h"
