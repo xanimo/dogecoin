d2559fa867b3e00361fb4cfa0980d410fc957973
Matt Corallo <git@bluematt.me>
2017-01-19 16:49:22 -0500

    Give CMainSignals a reference to the global scheduler
    ...so that it can run some signals in the background later
    
    Cherry-picked from: cda1429d5bfee129a0d1f6f1c65962b30251bafb

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -4,6 +4,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "validationinterface.h"
+#include "init.h"
+#include "scheduler.h"
 
 #include <boost/signals2/signal.hpp>
 
@@ -18,12 +20,23 @@ struct MainSignalsInstance {
     boost::signals2::signal<void (const CBlock&, const CValidationState&)> BlockChecked;
     boost::signals2::signal<void (std::shared_ptr<CReserveScript>&)> ScriptForMining;
     boost::signals2::signal<void (const CBlockIndex *, const std::shared_ptr<const CBlock>&)> NewPoWValidBlock;
+
+    CScheduler *m_scheduler = NULL;
 };
 
 static CMainSignals g_signals;
 
 CMainSignals::CMainSignals() {
     m_internals.reset(new MainSignalsInstance());
+}
+
+void CMainSignals::RegisterBackgroundSignalScheduler(CScheduler& scheduler) {
+    assert(!m_internals->m_scheduler);
+    m_internals->m_scheduler = &scheduler;
+}
+
+void CMainSignals::UnregisterBackgroundSignalScheduler() {
+    m_internals->m_scheduler = NULL;
 }
 
 CMainSignals& GetMainSignals()
