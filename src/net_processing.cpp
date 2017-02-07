9ccc74829c83df4456ac3ba99370b5857c4f4316
Matt Corallo <git@bluematt.me>
2017-02-07 11:49:07 -0500

    Add comment about cs_most_recent_block coverage

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -855,6 +855,7 @@ void PeerLogicValidation::SyncTransaction(const CTransaction& tx, const CBlockIn
     g_txrequest.ForgetTxHash(tx.GetHash());
 }
 
+// All of the following cache a recent block, and are protected by cs_most_recent_block
 static CCriticalSection cs_most_recent_block;
 static std::shared_ptr<const CBlock> most_recent_block;
 static std::shared_ptr<const CBlockHeaderAndShortTxIDs> most_recent_compact_block;
