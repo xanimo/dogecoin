16e224fd945bdb4411b2f2a55a7603edbe42a644
Gregory Sanders <gsanders87@gmail.com>
2017-03-02 17:04:39 -0800

    Disallow copy of CReserveKeys

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -993,6 +993,10 @@ public:
         pwallet = pwalletIn;
     }
 
+    CReserveKey() = default;
+    CReserveKey(const CReserveKey&) = delete;
+    CReserveKey& operator=(const CReserveKey&) = delete;
+
     ~CReserveKey()
     {
         ReturnKey();
