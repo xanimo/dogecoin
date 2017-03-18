bc03aa6d632fea7390e36060599dac12a3f7b175
practicalswift <practicalswift@users.noreply.github.com>
2017-03-18 12:40:58 +0100

    Set to nullptr after delete

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -477,6 +477,7 @@ void StopHTTPServer()
         LogPrint("http", "Waiting for HTTP worker threads to exit\n");
         workQueue->WaitExit();
         delete workQueue;
+        workQueue = nullptr;
     }
     if (eventBase) {
         LogPrint("http", "Waiting for HTTP event thread to exit\n");
