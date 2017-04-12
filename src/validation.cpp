3c2193d9485752901d671797bb1a2e22c16765f3
Jorge Timón <jtimon@jtimon.cc>
2017-04-13 00:51:39 +0200

    scripted-diff: Remove BOOST_REVERSE_FOREACH
    -BEGIN VERIFY SCRIPT-
    sed -i 's/BOOST_REVERSE_FOREACH(\(.*\), \(.*\))/for (\1 : reverse_iterate(\2))/' ./src/*.h ./src/*.cpp ./src/*/*.h ./src/*/*.cpp ;
    -END VERIFY SCRIPT-
    
    Cherry-picked from: 3eff827

--- src/validation.cpp
+++ src/validation.cpp
@@ -2466,7 +2466,7 @@ static bool ActivateBestChainStep(CValidationState& state, const CChainParams& c
         nHeight = nTargetHeight;
 
         // Connect new blocks.
-        BOOST_REVERSE_FOREACH(CBlockIndex *pindexConnect, vpindexToConnect) {
+        for (CBlockIndex *pindexConnect : reverse_iterate(vpindexToConnect)) {
             if (!ConnectTip(state, chainparams, pindexConnect, pindexConnect == pindexMostWork ? pblock : std::shared_ptr<const CBlock>(), connectTrace)) {
                 if (state.IsInvalid()) {
                     // The block violates a consensus rule.
