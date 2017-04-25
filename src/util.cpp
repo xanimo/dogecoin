2b30d0c697ac267b41e000651c2536f56588e3a8
fanquake <fanquake@gmail.com>
2017-04-25 09:34:23 +0800

    Use std::thread::hardware_concurrency, instead of Boost, to determine available cores
    Cherry-picked from: 937bf4335bc58c443645dc29b8d7ceadc81e74e5

--- src/util.cpp
+++ src/util.cpp
@@ -87,6 +87,7 @@
 #include <openssl/crypto.h>
 #include <openssl/rand.h>
 #include <openssl/conf.h>
+#include <thread>
 
 using namespace std;
 
@@ -856,11 +857,7 @@ bool SetupNetworking()
 
 int GetNumCores()
 {
-#if BOOST_VERSION >= 105600
-    return boost::thread::physical_concurrency();
-#else // Must fall back to hardware_concurrency, which unfortunately counts virtual cores
-    return boost::thread::hardware_concurrency();
-#endif
+    return std::thread::hardware_concurrency();
 }
 
 std::string CopyrightHolders(const std::string& strPrefix)
