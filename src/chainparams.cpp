c4fa908939b16c6e049d8d0bc40623969b4e77a7
Cory Fields <cory-nospam-@coryfields.com>
2017-05-23 20:48:08 -0400

    net: switch to dummy internal ip for dns seed source
    This addresss the TODO to avoid resolving twice.
    
    Cherry-picked from: 6cdc488e3604267b1e115da5c50ffb970cd30084

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,8 +161,8 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x5b2a3f53f605d62c53e62932dac6925e3d74afa5a4b459745c36d42d0ed26a69"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.push_back(CDNSSeedData("multidoge.org", "seed.multidoge.org", true));
-        vSeeds.push_back(CDNSSeedData("multidoge.org", "seed2.multidoge.org"));
+        vSeeds.emplace_back("seed.multidoge.org", true);
+        vSeeds.emplace_back("seed2.multidoge.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,22);
@@ -322,7 +322,7 @@ public:
 
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.push_back(CDNSSeedData("jrn.me.uk", "testseed.jrn.me.uk"));
+        vSeeds.emplace_back("testseed.jrn.me.uk");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,113); // 0x71
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196); // 0xc4
