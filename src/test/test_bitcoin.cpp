27181162d1219e794f540abd9c23c797774ee5cf
Pieter Wuille <pieter.wuille@gmail.com>
2017-07-13 23:26:04 -0700

    Add SHA256 dispatcher
    Cherry-picked from: 2991c91

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -10,6 +10,7 @@
 #include "chainparams.h"
 #include "consensus/consensus.h"
 #include "consensus/validation.h"
+#include "crypto/sha256.h"
 #include "key.h"
 #include "validation.h"
 #include "miner.h"
@@ -42,6 +43,7 @@ static const int COINBASE_MATURITY = 60*4; // 4 hours of blocks
 
 BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
 {
+        SHA256AutoDetect();
         ECC_Start();
         SetupEnvironment();
         SetupNetworking();
