ce3efc507db4eccef4326a45218919c4ea77f803
Matt Corallo <git@bluematt.me>
2017-10-02 18:24:59 -0400

    Fix validationinterface build on super old boost/clang
    This should fix all the non-dependancy issues for termux builds.
    See Github issue #11388.
    
    Cherry-picked from: 96c2ce9d343147608c197bc2333138b82141340f

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -5,7 +5,9 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "validationinterface.h"
+
 #include "init.h"
+#include "primitives/block.h"
 #include "scheduler.h"
 
 #include <boost/bind/bind.hpp>
