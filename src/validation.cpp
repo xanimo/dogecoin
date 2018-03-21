2240011a8540b92595dee8292b983b205aa5c9e0
Pieter Wuille <pieter.wuille@gmail.com>
2018-03-20 17:37:32 -0700

    Add native support for serializing char arrays without FLATDATA
    Support is added to serialize arrays of type char or unsigned char directly,
    without any wrappers. All invocations of the FLATDATA wrappers that are
    obsoleted by this are removed.
    
    This includes a patch by Russell Yanofsky to make char casting type safe.
    
    The serialization of CSubNet is changed to serialize a bool directly rather
    than though FLATDATA. This makes the serialization independent of the size
    of the bool type (and will use 1 byte everywhere).

--- src/validation.cpp
+++ src/validation.cpp
@@ -1107,7 +1107,7 @@ bool WriteBlockToDisk(const CBlock& block, CDiskBlockPos& pos, const CMessageHea
 
     // Write index header
     unsigned int nSize = GetSerializeSize(fileout, block);
-    fileout << FLATDATA(messageStart) << nSize;
+    fileout << messageStart << nSize;
 
     // Write block
     long fileOutPos = ftell(fileout.Get());
@@ -1481,7 +1481,7 @@ bool UndoWriteToDisk(const CBlockUndo& blockundo, CDiskBlockPos& pos, const uint
 
     // Write index header
     unsigned int nSize = GetSerializeSize(fileout, blockundo);
-    fileout << FLATDATA(messageStart) << nSize;
+    fileout << messageStart << nSize;
 
     // Write undo data
     long fileOutPos = ftell(fileout.Get());
@@ -4041,7 +4041,7 @@ bool LoadExternalBlockFile(const CChainParams& chainparams, FILE* fileIn, CDiskB
                 unsigned char buf[CMessageHeader::MESSAGE_START_SIZE];
                 blkdat.FindByte(chainparams.MessageStart()[0]);
                 nRewind = blkdat.GetPos()+1;
-                blkdat >> FLATDATA(buf);
+                blkdat >> buf;
                 if (memcmp(buf, chainparams.MessageStart(), CMessageHeader::MESSAGE_START_SIZE))
                     continue;
                 // read size
