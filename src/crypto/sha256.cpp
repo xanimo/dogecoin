6b72a936f3a3b597873facb2e0b5ec868e99eef8
Pieter Wuille <pieter.wuille@gmail.com>
2018-05-08 10:27:57 -0700

    4-way SSE4.1 implementation for double SHA256 on 64-byte inputs
    Cherry-picked from: 230294b

--- src/crypto/sha256.cpp
+++ src/crypto/sha256.cpp
@@ -66,6 +66,16 @@ void Transform(uint32_t* s, const unsigned char* chunk, size_t blocks);
 #endif
 #endif
 
+namespace sha256d64_sse41
+{
+void Transform_4way(unsigned char* out, const unsigned char* in);
+}
+
+namespace sha256d64_avx2
+{
+void Transform_8way(unsigned char* out, const unsigned char* in);
+}
+
 // Internal implementation code.
 namespace
 {
@@ -637,6 +647,8 @@ typedef void (*transform_d64_type)(unsigned char*, const unsigned char*);
 /** Define a function pointer for Transform */
 transform_type transform_ptr = sha256::Transform;
 transform_d64_type transfrom_ptr_d64 = sha256::TransformD64;
+transform_d64_type transfrom_ptr_d64_4way = nullptr;
+transform_d64_type transfrom_ptr_d64_8way = nullptr;
 
 template<transform_type tr>
 void TransformD64Wrapper(unsigned char* out, const unsigned char* in)
@@ -679,6 +691,14 @@ void TransformD64Wrapper(unsigned char* out, const unsigned char* in)
 
 } // namespace sha256
 
+#if defined(USE_ASM) && (defined(__x86_64__) || defined(__amd64__))
+// We can't use cpuid.h's __get_cpuid as it does not support subleafs.
+void inline cpuid(uint32_t leaf, uint32_t subleaf, uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d)
+{
+  __asm__ ("cpuid" : "=a"(a), "=b"(b), "=c"(c), "=d"(d) : "0"(leaf), "2"(subleaf));
+}
+#endif
+
 /** Initialize the function pointer */
 void inline Initialize_transform_ptr(void)
 {
@@ -698,9 +718,20 @@ void inline Initialize_transform_ptr(void)
 #endif
 #if defined(USE_ASM) && (defined(__x86_64__) || defined(__amd64__))
     uint32_t eax, ebx, ecx, edx;
-    if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) && (ecx >> 19) & 1)
+    cpuid(1, 0, eax, ebx, ecx, edx);
+    if ((ecx >> 19) & 1) {
         sha256::transform_ptr = sha256_sse4::Transform;
         sha256::transfrom_ptr_d64 = sha256::TransformD64Wrapper<sha256_sse4::Transform>;
+#if defined(ENABLE_SSE41)
+        sha256::transfrom_ptr_d64_4way = sha256d64_sse41::Transform_4way;
+#endif
+#if defined(ENABLE_AVX2)
+        cpuid(7, 0, eax, ebx, ecx, edx);
+        if ((ebx >> 5) & 1) {
+            sha256::transfrom_ptr_d64_8way = sha256d64_avx2::Transform_8way;
+        }
+#endif
+    }
 #endif
 }
 
@@ -765,6 +796,22 @@ CSHA256& CSHA256::Reset()
 
 void SHA256D64(unsigned char* out, const unsigned char* in, size_t blocks)
 {
+    if (sha256::transfrom_ptr_d64_8way) {
+        while (blocks >= 8) {
+            sha256::transfrom_ptr_d64_8way(out, in);
+            out += 256;
+            in += 512;
+            blocks -= 8;
+        }
+    }
+    if (sha256::transfrom_ptr_d64_4way) {
+        while (blocks >= 4) {
+            sha256::transfrom_ptr_d64_4way(out, in);
+            out += 128;
+            in += 256;
+            blocks -= 4;
+        }
+    }
     while (blocks) {
         sha256::transfrom_ptr_d64(out, in);
         out += 32;
