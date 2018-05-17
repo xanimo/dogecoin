d4a1507e90ec23194ce67118e39982ef8fd3c852
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2018-05-17 14:50:15 +0900

    uint256: Remove unnecessary crypto/common.h use

--- src/validation.h
+++ src/validation.h
@@ -14,6 +14,7 @@
 #include "amount.h"
 #include "chain.h"
 #include "coins.h"
+#include <crypto/common.h> // for ReadLE64
 #include "policy/policy.h" // For RECOMMENDED_MIN_TX_FEE
 #include "fs.h"
 #include "protocol.h" // For CMessageHeader::MessageStartChars
@@ -158,7 +159,10 @@ static const bool DEFAULT_PEERBLOOMFILTERS = true;
 
 struct BlockHasher
 {
-    size_t operator()(const uint256& hash) const { return hash.GetCheapHash(); }
+    // this used to call `GetCheapHash()` in uint256, which was later moved; the
+    // cheap hash function simply calls ReadLE64() however, so the end result is
+    // identical
+    size_t operator()(const uint256& hash) const { return ReadLE64(hash.begin()); }
 };
 
 extern CScript COINBASE_FLAGS;
