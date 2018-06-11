efee843648f2c9f6eeeaefde5715ef3a6f292e08
Pieter Wuille <pieter.wuille@gmail.com>
2018-06-11 09:59:20 -0700

    Use immintrin.h everywhere for intrinsics
    Cherry-picked from: 66b2cf1ccfad545a8ec3f2a854e23f647322bf30

--- src/test/crypto_tests.cpp
+++ src/test/crypto_tests.cpp
@@ -551,7 +551,6 @@ BOOST_AUTO_TEST_CASE(sha256d64)
         SHA256D64(out2, in, i);
         printf("%02X\n", out1);
         printf("%02X\n", out2);
-        printf("%d\n", 32 * i);
         printf("%d\n", memcmp(out1, out2, 32 * i));
         BOOST_CHECK(memcmp(out1, out2, 32 * i) == 0);
     }
