5590a90581d8f7d07fed4294ad5c566a7b8551f9
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-08-07 20:25:42 +0200

    util: make EncodeBase64 consume Spans
    Cherry-picked from: e2aa1a5

--- src/utilstrencodings.h
+++ src/utilstrencodings.h
@@ -47,7 +47,7 @@ signed char HexDigit(char c);
 bool IsHex(const std::string& str);
 std::vector<unsigned char> DecodeBase64(const char* p, bool* pfInvalid = NULL);
 std::string DecodeBase64(const std::string& str);
-std::string EncodeBase64(const unsigned char* pch, size_t len);
+std::string EncodeBase64(Span<const unsigned char> input);
 std::string EncodeBase64(const std::string& str);
 std::vector<unsigned char> DecodeBase32(const char* p, bool* pfInvalid = NULL);
 std::string DecodeBase32(const std::string& str);
