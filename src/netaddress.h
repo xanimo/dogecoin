14478674fd89ee1c57ca4e5c4bf25ec7abb78be4
Pieter Wuille <pieter@wuille.net>
2020-10-12 15:35:08 -0700

    Ignore incorrectly-serialized banlist.dat entries
    Cherry-picked from: 886be97

--- src/netaddress.h
+++ src/netaddress.h
@@ -454,6 +454,8 @@ class CSubNet
         /// Is this value valid? (only used to signal parse errors)
         bool valid;
 
+        bool SanityCheck() const;
+
     public:
         CSubNet();
         CSubNet(const CNetAddr& addr, uint8_t mask);
@@ -485,6 +487,8 @@ class CSubNet
                 READWRITE(obj.netmask);
             }
             READWRITE(obj.valid);
+            // Mark invalid if the result doesn't pass sanity checking.
+            SER_READ(obj, if (obj.valid) obj.valid = obj.SanityCheck());
         }
 };
 
