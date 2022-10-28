8b652b32ec7966b808ad4a5b18bed60f122fd8b6
Dakoda Greaves <dakoda@xanimo.net>
2022-10-27 20:41:06 -0700

    scripted-diff: Remove BOOST_FOREACH
    -BEGIN VERIFY SCRIPT-
    sed -i 's/BOOST_FOREACH(\(.*\), \(.*\))/for (\1 : \2)/' ./src/*.h ./src/*.cpp ./src/*/*.h ./src/*/*.cpp ./src/*/*/*.cpp;
    sed -i 's/BOOST_FOREACH (\(.*\), \(.*\))/for (\1 : \2)/' ./src/*.h ./src/*.cpp ./src/*/*.h ./src/*/*.cpp ./src/*/*/*.cpp;
    -END VERIFY SCRIPT-

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -54,7 +54,7 @@ int64_t static DecodeDumpTime(const std::string &str) {
 
 std::string static EncodeDumpString(const std::string &str) {
     std::stringstream ret;
-    BOOST_FOREACH(unsigned char c, str) {
+    for (unsigned char c : str) {
         if (c <= 32 || c >= 128 || c == '%') {
             ret << '%' << HexStr(&c, &c + 1);
         } else {
@@ -1078,7 +1078,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
 
     UniValue response(UniValue::VARR);
 
-    BOOST_FOREACH (const UniValue& data, requests.getValues()) {
+    for (const UniValue& data : requests.getValues()) {
         const int64_t timestamp = std::max(GetImportTimestamp(data, now), minimumTimestamp);
         const UniValue result = ProcessImport(data, timestamp);
         response.push_back(result);
