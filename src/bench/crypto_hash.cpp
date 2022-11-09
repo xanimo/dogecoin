theStack/bitcoin

Add SHA3 benchmark

src/bench/crypto_hash.cpp
@@ -7,6 +7,7 @@
 #include <crypto/ripemd160.h>
 #include <crypto/sha1.h>
 #include <crypto/sha256.h>
+#include <crypto/sha3.h>
 #include <crypto/sha512.h>
 #include <crypto/siphash.h>
 #include <hash.h>
@@ -40,6 +41,15 @@ static void SHA256(benchmark::Bench& bench)
     std::vector<uint8_t> in(BUFFER_SIZE,0);
     bench.batch(in.size()).unit("byte").run([&] {
         CSHA256().Write(in.data(), in.size()).Finalize(hash);
+    });
+}
+
+static void SHA3_256_1M(benchmark::Bench& bench)
+{
+    uint8_t hash[SHA3_256::OUTPUT_SIZE];
+    std::vector<uint8_t> in(BUFFER_SIZE,0);
+    bench.batch(in.size()).unit("byte").run([&] {
+        SHA3_256().Write(in).Finalize(hash);
     });
 }
 
@@ -99,6 +109,7 @@ BENCHMARK(RIPEMD160);
 BENCHMARK(SHA1);
 BENCHMARK(SHA256);
 BENCHMARK(SHA512);
+BENCHMARK(SHA3_256_1M);
 
 BENCHMARK(SHA256_32b);
 BENCHMARK(SipHash_32b);

xanimo/dogecoin

Cherry-picked from: cb0e238
Original commit: 3f01ddb

src/bench/crypto_hash.cpp
@@ -12,6 +12,7 @@
 #include "utiltime.h"
 #include "crypto/ripemd160.h"
 #include "crypto/sha1.h"
+#include "crypto/sha3.h"
 #include "crypto/sha256.h"
 #include "crypto/sha512.h"
 
@@ -40,6 +41,14 @@ static void SHA256(benchmark::State& state)
     std::vector<uint8_t> in(BUFFER_SIZE,0);
     while (state.KeepRunning())
         CSHA256().Write(in.data(), in.size()).Finalize(hash);
+}
+
+static void SHA3_256_1M(benchmark::State& state)
+{
+    uint8_t hash[SHA3_256::OUTPUT_SIZE];
+    std::vector<uint8_t> in(BUFFER_SIZE,0);
+    while (state.KeepRunning())
+        SHA3_256().Write(in).Finalize(hash);
 }
 
 static void SHA256_32b(benchmark::State& state)
@@ -104,6 +113,7 @@ BENCHMARK(RIPEMD160);
 BENCHMARK(SHA1);
 BENCHMARK(SHA256);
 BENCHMARK(SHA512);
+BENCHMARK(SHA3_256_1M);
 
 BENCHMARK(SHA256_32b);
 BENCHMARK(SipHash_32b);