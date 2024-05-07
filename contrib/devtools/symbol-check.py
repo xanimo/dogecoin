3582096982daa2315e3d5a3f9aa3533cffae98eb
Dakoda Greaves <dakoda@xanimo.net>
2024-05-06 21:23:40 -0700

    build: Disallow dynamic linking against c++ library
    Ever since statically linking Qt, we've been linking the C++ library
    statically too (-static-libstdc++). Take this into
    account in the symbol checker.
    
    Cherry-picked from: b77d5ad59fb9f3f26d919ee6c33ae732382de504

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -3,8 +3,14 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
+<<<<<<< HEAD
 A script to check that release executables only contain certain symbols
 and are only linked against allowed libraries.
+=======
+A script to check that the (Linux) executables produced by gitian only contain
+allowed gcc and glibc version symbols. This makes sure they are still compatible
+with the minimum supported Linux distribution versions.
+>>>>>>> b77d5ad59f (build: Disallow dynamic linking against c++ library)
 
 Example usage:
 
@@ -22,19 +28,16 @@ import lief #type:ignore
 #
 # - g++ version 4.4.5 (https://packages.debian.org/search?suite=default&section=all&arch=any&searchon=names&keywords=g%2B%2B)
 # - libc version 2.11.3 (https://packages.debian.org/search?suite=default&section=all&arch=any&searchon=names&keywords=libc6)
-# - libstdc++ version 4.4.5 (https://packages.debian.org/search?suite=default&section=all&arch=any&searchon=names&keywords=libstdc%2B%2B6)
 #
 # Ubuntu 10.04.4 (Lucid Lynx) has:
 #
 # - g++ version 4.4.3 (http://packages.ubuntu.com/search?keywords=g%2B%2B&searchon=names&suite=lucid&section=all)
 # - libc version 2.11.1 (http://packages.ubuntu.com/search?keywords=libc6&searchon=names&suite=lucid&section=all)
-# - libstdc++ version 4.4.3 (http://packages.ubuntu.com/search?suite=lucid&section=all&arch=any&keywords=libstdc%2B%2B&searchon=names)
 #
 # Taking the minimum of these as our target.
 #
 # According to GNU ABI document (http://gcc.gnu.org/onlinedocs/libstdc++/manual/abi.html) this corresponds to:
 #   GCC 4.4.0: GCC_4.4.0
-#   GCC 4.4.2: GLIBCXX_3.4.13, CXXABI_1.3.3
 #   (glibc)    GLIBC_2_11
 
 MAX_VERSIONS = {
@@ -47,8 +50,6 @@ MAX_VERSIONS = {
     lief.ELF.ARCH.PPC64:  (2,17),
     lief.ELF.ARCH.RISCV:  (2,27),
 },
-'CXXABI':  (1,3,3),
-'GLIBCXX': (3,4,13),
 'V':         (0,5,0),  # xkb (dogecoin-qt only)
 }
 
