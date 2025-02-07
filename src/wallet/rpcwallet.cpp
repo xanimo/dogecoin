ed8c350e42d587e7b8b7affa8423a7c1d3053010
xanimo <dakoda@xanimo.net>
2025-02-06 21:28:11 -0800

    scripted-diff: Remove PAIRTYPE
    -BEGIN VERIFY SCRIPT-
    sed -i 's/PAIRTYPE(\([^,]*\), \([^\)]*\))/std::pair<\1, \2>/' ./src/*.h ./src/*.cpp ./src/*/*.h ./src/*/*.cpp ./src/*/*/*.h ./src/*/*/*.cpp ;
    sed -i ':a;N;$!ba;s/#define std::pair<t1, t2>    std::pair<t1, t2>\n//' ./src/utilstrencodings.h ;
    -END VERIFY SCRIPT-

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -94,7 +94,7 @@ void WalletTxToJSON(const CWalletTx& wtx, UniValue& entry)
     }
     entry.pushKV("bip125-replaceable", rbfStatus);
 
-    for (const PAIRTYPE(string,string) item : wtx.mapValue)
+    for (const std::pair<std::string, std::string> item : wtx.mapValue)
         entry.pushKV(item.first, item.second);
 }
 
@@ -330,8 +330,7 @@ UniValue getaddressesbyaccount(const JSONRPCRequest& request)
 
     // Find all addresses that have the given account
     UniValue ret(UniValue::VARR);
-    for (const PAIRTYPE(CBitcoinAddress, CAddressBookData) item : pwalletMain->mapAddressBook)
-    {
+    for (const std::pair<CBitcoinAddress, CAddressBookData> item : pwalletMain->mapAddressBook) {
         const CBitcoinAddress& address = item.first;
         const string& strName = item.second.name;
         if (strName == strAccount)
@@ -1275,9 +1274,8 @@ UniValue ListReceived(const UniValue& params, bool fByAccounts)
 
     // Reply
     UniValue ret(UniValue::VARR);
-    map<string, tallyitem> mapAccountTally;
-    for (const PAIRTYPE(CBitcoinAddress, CAddressBookData) item : pwalletMain->mapAddressBook)
-    {
+    std::map<std::string, tallyitem> mapAccountTally;
+    for (const std::pair<CBitcoinAddress, CAddressBookData> item : pwalletMain->mapAddressBook) {
         const CBitcoinAddress& address = item.first;
         const string& strAccount = item.second.name;
         map<CBitcoinAddress, tallyitem>::iterator it = mapTally.find(address);
@@ -1797,8 +1795,8 @@ UniValue listaccounts(const JSONRPCRequest& request)
         if(request.params[1].get_bool())
             includeWatchonly = includeWatchonly | ISMINE_WATCH_ONLY;
 
-    map<string, CAmount> mapAccountBalances;
-    for (const PAIRTYPE(CTxDestination, CAddressBookData) entry : pwalletMain->mapAddressBook) {
+    std::map<std::string, CAmount> mapAccountBalances;
+    for (const std::pair<CTxDestination, CAddressBookData> entry : pwalletMain->mapAddressBook) {
         if (IsMine(*pwalletMain, entry.first) & includeWatchonly) // This address belongs to me
             mapAccountBalances[entry.second.name] = 0;
     }
@@ -1832,7 +1830,7 @@ UniValue listaccounts(const JSONRPCRequest& request)
         mapAccountBalances[entry.strAccount] += entry.nCreditDebit;
 
     UniValue ret(UniValue::VOBJ);
-    for (const PAIRTYPE(string, CAmount) accountBalance : mapAccountBalances) {
+    for (const std::pair<std::string, CAmount> accountBalance : mapAccountBalances) {
         ret.pushKV(accountBalance.first, ValueFromAmount(accountBalance.second));
     }
     return ret;
