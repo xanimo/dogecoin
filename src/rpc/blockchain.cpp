0302341ee02a743c9ceed6422efb1b2c8efcb9a4
Russell Yanofsky <russ@yanofsky.org>
2026-03-30 11:27:15 -0700

    Add static_assert to prevent VARINT(<signed value>)
    Adds VarIntMode enum (DEFAULT for unsigned, NONNEGATIVE_SIGNED for signed).
    All callers with signed types annotated with VarIntMode::NONNEGATIVE_SIGNED.
    
    Adapted for Dogecoin: added VarIntMode to CCoins nVersion/nHeight,
    CTxInUndo nVersion, and rpc/blockchain.cpp VARINT(0u).
    
    Cherry-picked from bitcoin/bitcoin@499d95e278.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -900,7 +900,7 @@ static bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
                 }
             }
             stats.nSerializedSize += 32 + pcursor->GetValueSize();
-            ss << VARINT(0);
+            ss << VARINT(0u);
         } else {
             return error("%s: unable to read value", __func__);
         }
