1c5da9b484d0eb99f61bb6ca98c914dc3ad7a10a
xanimo <bluezr@dogecoin.com>
2026-05-09 20:38:18 -0700

    [net] Signal NODE_COMPACT_FILTERS if we're serving compact filters.
    If -peerblockfilters is configured, signal the NODE_COMPACT_FILTERS service
    bit to indicate that we are able to serve compact block filters, headers
    and checkpoints.
    
    
    Cherry-picked from: 132b30d9c84f2a8053714a438f227b583a89a9ea

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1394,7 +1394,7 @@ static bool PrepareBlockFilterRequest(CNode& peer, const CChainParams& chain_par
 {
     const bool supported_filter_type =
         (filter_type == BlockFilterType::BASIC &&
-         GetBoolArg("-peerblockfilters", DEFAULT_PEERBLOCKFILTERS));
+         (peer.GetLocalServices() & NODE_COMPACT_FILTERS));
     if (!supported_filter_type) {
         LogPrint("net", "peer %d requested unsupported block filter type: %d\n",
                  peer.id, static_cast<uint8_t>(filter_type));
