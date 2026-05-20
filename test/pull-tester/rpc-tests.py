tmagik/catoshi

Rename qa directory to test

test/pull-tester/rpc-tests.py
@@ -11,7 +11,7 @@ forward all unrecognized arguments onto the individual test scripts.
 RPC tests are disabled on Windows by default. Use --force to run them anyway.
 
 For a description of arguments recognized by test scripts, see
-`qa/pull-tester/test_framework/test_framework.py:BitcoinTestFramework.main`.
+`test/pull-tester/test_framework/test_framework.py:BitcoinTestFramework.main`.
 
 """
 
@@ -171,7 +171,7 @@ def main():
             import zmq
         except ImportError:
             print("ERROR: \"import zmq\" failed. Use -nozmq to run without the ZMQ tests."
-                  "To run zmq tests, see dependency info in /qa/README.md.")
+                  "To run zmq tests, see dependency info in /test/README.md.")
             raise
 
     # Build list of tests
@@ -206,7 +206,7 @@ def main():
     if args.help:
         # Print help for rpc-tests.py, then print help of the first script and exit.
         parser.print_help()
-        subprocess.check_call((config["environment"]["SRCDIR"] + '/qa/rpc-tests/' + test_list[0]).split() + ['-h'])
+        subprocess.check_call((config["environment"]["SRCDIR"] + '/test/rpc-tests/' + test_list[0]).split() + ['-h'])
         sys.exit(0)
 
     run_tests(test_list, config["environment"]["SRCDIR"], config["environment"]["BUILDDIR"], config["environment"]["EXEEXT"], args.jobs, args.coverage, passon_args)
@@ -222,10 +222,10 @@ def run_tests(test_list, src_dir, build_dir, exeext, jobs=1, enable_coverage=Fal
     if "BITCOIND" not in os.environ:
         os.environ["BITCOIND"] = build_dir + '/src/bitcoind' + exeext
 
-    tests_dir = src_dir + '/qa/rpc-tests/'
+    tests_dir = src_dir + '/test/rpc-tests/'
 
     flags = ["--srcdir={}/src".format(build_dir)] + args
-    flags.append("--cachedir=%s/qa/cache" % build_dir)
+    flags.append("--cachedir=%s/test/cache" % build_dir)
 
     if enable_coverage:
         coverage = RPCCoverage()
@@ -335,7 +335,7 @@ class RPCCoverage(object):
     After all tests complete, the commands run are combined and diff'd against
     the complete list to calculate uncovered RPC commands.
 
-    See also: qa/rpc-tests/test_framework/coverage.py
+    See also: test/rpc-tests/test_framework/coverage.py
 
     """
     def __init__(self):
@@ -363,7 +363,7 @@ class RPCCoverage(object):
         Return a set of currently untested RPC commands.
 
         """
-        # This is shared from `qa/rpc-tests/test-framework/coverage.py`
+        # This is shared from `test/rpc-tests/test-framework/coverage.py`
         reference_filename = 'rpc_interface.txt'
         coverage_file_prefix = 'coverage.'
 

xanimo/dogecoin

(cherry picked from commit 00902c48cdc5d6a531abfd279fd8e7779cf0a0b8)

test/pull-tester/rpc-tests.py
@@ -18,7 +18,7 @@ than:
       interface.
 
 For a description of arguments recognized by test scripts, see
-`qa/pull-tester/test_framework/test_framework.py:BitcoinTestFramework.main`.
+`test/pull-tester/test_framework/test_framework.py:BitcoinTestFramework.main`.
 
 """
 
@@ -30,7 +30,7 @@ import subprocess
 import tempfile
 import re
 
-sys.path.append("qa/pull-tester/")
+sys.path.append("test/pull-tester/")
 from tests_config import *
 
 BOLD = ("","")
@@ -39,7 +39,7 @@ if os.name == 'posix':
     # terminal via ANSI escape sequences:
     BOLD = ('\033[0m', '\033[1m')
 
-RPC_TESTS_DIR = SRCDIR + '/qa/rpc-tests/'
+RPC_TESTS_DIR = SRCDIR + '/test/rpc-tests/'
 
 #If imported values are not defined then set to zero (or disabled)
 if 'ENABLE_WALLET' not in vars():
@@ -95,7 +95,7 @@ if ENABLE_ZMQ:
         import zmq
     except ImportError:
         print("ERROR: \"import zmq\" failed. Set ENABLE_ZMQ=0 or "
-              "to run zmq tests, see dependency info in /qa/README.md.")
+              "to run zmq tests, see dependency info in /test/README.md.")
         # ENABLE_ZMQ=0
         raise
 
@@ -240,7 +240,7 @@ def runtests():
         coverage = RPCCoverage()
         print("Initializing coverage directory at %s\n" % coverage.dir)
     flags = ["--srcdir=%s/src" % BUILDDIR] + passon_args
-    flags.append("--cachedir=%s/qa/cache" % BUILDDIR)
+    flags.append("--cachedir=%s/test/cache" % BUILDDIR)
     if coverage:
         flags.append(coverage.flag)
 
@@ -341,7 +341,7 @@ class RPCCoverage(object):
     After all tests complete, the commands run are combined and diff'd against
     the complete list to calculate uncovered RPC commands.
 
-    See also: qa/rpc-tests/test_framework/coverage.py
+    See also: test/rpc-tests/test_framework/coverage.py
 
     """
     def __init__(self):
@@ -369,7 +369,7 @@ class RPCCoverage(object):
         Return a set of currently untested RPC commands.
 
         """
-        # This is shared from `qa/rpc-tests/test-framework/coverage.py`
+        # This is shared from `test/rpc-tests/test-framework/coverage.py`
         REFERENCE_FILENAME = 'rpc_interface.txt'
         COVERAGE_FILE_PREFIX = 'coverage.'
 