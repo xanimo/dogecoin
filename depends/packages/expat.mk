70b2ecc05de71958d68e4757ec6cb3d4df9df0bd
xanimo <bluezr@dogecoin.com>
2026-06-14 11:43:35 -0700

    depends: fix expat download URL (sourceforge 404)
    Switch to GitHub release mirror for expat-2.6.2; SHA256 matches.

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,6 +1,6 @@
 package=expat
 $(package)_version=2.6.2
-$(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/$($(package)_version)
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_6_2
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=9C7C1B5DCBC3C237C500A8FB1493E14D9582146DD9B42AA8D3FFB856A3B927E0
 
