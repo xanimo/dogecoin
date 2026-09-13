32af7554030ac3eb9131a37d6c77d4a80480d7fb
xanimo <bluezr@dogecoin.com>
2026-09-12 17:38:05 -0700

    util: drop openssl includes left dead by the CInit move
    RNGState took over the locking callbacks and OPENSSL_no_config, so crypto.h,
    rand.h and conf.h have no users left in this file.
    
    Builds clean; random_tests pass.

--- src/util.cpp
+++ src/util.cpp
@@ -85,9 +85,6 @@
 #include <boost/program_options/detail/config_file.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/thread.hpp>
-#include <openssl/crypto.h>
-#include <openssl/rand.h>
-#include <openssl/conf.h>
 
 // Work around clang compilation problem in Boost 1.46:
 // /usr/include/boost/program_options/detail/config_file.hpp:163:17: error: call to function 'to_internal' that is neither visible in the template definition nor found by argument-dependent lookup
